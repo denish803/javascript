d7972097c182f64b176a780e0ede7b367c10ebd7
denish kotadiya <denishkotadiya204@gmail.com>
2025-09-27 15:29:31 +0530

    Add examples of JavaScript comparison operators
    Introduced a new file demonstrating various JavaScript comparison operators, including equality, strict equality, and relational operators, with console output and explanatory comments.

---
+++ 01_Basic/04_compariasion.js
@@ -0,0 +1,21 @@
+console.log(10 > 1); // it will return true because 10 is greater than 1
+console.log(10 < 1); // it will return false because 10 is not less than 1
+console.log(10 >= 1); // it will return true because 10 is greater than 1
+console.log(10 <= 1); // it will return false because 10 is not less than 1
+console.log(10 == 10); // it will return true because both are equal
+console.log(10 != 10); // it will return false because both are equal
+console.log(10 === 10); // it will return true because both are equal and same datatype
+console.log(10 !== 10); // it will return false because both are equal and same datatype
+console.log(10 == "10"); // it will return true because both are equal but different datatype
+console.log(10 === "10"); // it will return false because both are equal but different datatype
+console.log(10 != "10"); // it will return false because both are equal but different datatype
+console.log(10 !== "10"); // it will return true because both are equal but different datatype
+
+console.log(true == 1); // it will return true because true is equal to 1
+console.log(false == 0);
+console.log(true === 1); // it will return false because true is not equal to 1
+console.log(false === 0);
+console.log(true != 1); // it will return false because true is equal to 1
+console.log(false != 0);
+console.log(true !== 1); // it will return true because true is not equal to 1
+console.log(false !== 0); // it will return true because false is not equal to 0
