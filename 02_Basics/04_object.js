7bc5c7074453af88810343af5882f1c5ab8d738f
denish kotadiya <denishkotadiya204@gmail.com>
2025-09-27 21:12:01 +0530

    basic object example

---
+++ 02_Basics/04_object.js
@@ -0,0 +1,36 @@
+const userDetials = {};
+
+userDetials.name = "John";
+userDetials.age = 30;
+userDetials.userPesionality = {
+    height: "5.9",
+    weight: "70kg",
+    working: true,
+    officeDetails: {
+        company: "ABC Corp",
+        position: "Developer"
+    }
+};
+
+// console.log(userDetials.userPesionality.officeDetails.company);
+
+const obj1 = {1: "one", 2: "two"};
+const obj2 = {3: "three", 4: "four"};
+
+const newObj1 = {obj1, obj2}; // it will create object inside object
+console.log(newObj1);
+
+const newObj2 = Object.assign({}, obj1, obj2); // it will merge two objects
+console.log(newObj2);
+
+const newObj = {...obj1, ...obj2}; // it will merge two objects
+console.log(newObj);
+
+console.log(Object.keys(newObj)); // it will return array of keys
+console.log(Object.values(newObj));
+
+console.log(Object.entries(userDetials)); // it will return array of key value pairs
+
+console.log(userDetials.hasOwnProperty("dsf"));
+
+
