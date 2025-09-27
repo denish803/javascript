e829c2a3abce33a8da4b66bbd37df6299d3e2ea1
denish kotadiya <denishkotadiya204@gmail.com>
2025-09-27 14:29:43 +0530

    02 learn Datatype

---
+++ 01_Basic/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict"; // it will help to write secure code and teke newr versoin of js
+
+// alert("hello world"); // it is not used in node js it is used in browser
+
+let name = "Rajesh Sharma"; // string datatype
+let age = 21; // number datatype
+let isApproved = true; // boolean datatype
+let firstName = undefined; // undefined datatype
+let selectedColor = null; // null datatype and null is object datatype
+let lastName = Symbol("Rajesh"); // symbol datatype it is used to create unique identifiers
+let bigNumber = 1234567890123456789012345678901234567890n; // bigint datatype it is used to store large numbers
+let person = { // object datatype
+    name: "Rajesh Sharma",
+    age: 21
+};
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isApproved);
+
+
