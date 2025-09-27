695924e0ba59064b855a34ac8c53e31af785880f
denish kotadiya <denishkotadiya204@gmail.com>
2025-09-27 17:25:01 +0530

    number and math example

---
+++ 01_Basic/05_number_math.js
@@ -0,0 +1,35 @@
+/*
+    let number = 123.551;
+
+    console.log("------------------------ Number --------------------------");
+
+    console.log(`Number: ${number}`);
+    console.log(`Type of number: ${typeof number}`);
+
+    console.log("--------------------------------------------------"); 
+
+    console.log(`Number of precistion : ${number.toPrecision(5)}`)
+    console.log(`Number of fixed : ${number.toFixed(2)}`)
+
+    console.log(`Number of show : ${Number(100000).toLocaleString()}`)
+
+*/
+
+console.log("------------------------ Math --------------------------");
+
+console.log(`Math.PI: ${Math.PI}`);
+console.log(`Math Mimume number $ ${Math.min(2, 3, 4, -5, -6, 7, 8, 9)}`); // min number identification
+console.log(`Math maximume number ${Math.max(2, 3, 4, -5, -6, 7, 8, 9)}`); // max number identification
+console.log(`Math square root $ ${Math.sqrt(25)}`); // square root
+console.log(`Math abs nagetive to positive number : ${Math.abs(-55)}`); // nagetive to positive conversion
+
+
+console.log(`Math power of number : ${Math.pow(2, 3)}`); // power of number
+
+
+console.log(`Math round of number : ${Math.round(2.5)}`); // round of number
+console.log(`Math ceil of numebr : ${Math.ceil(2.1)}`); // ceil of number means round up
+console.log(`Math floor of number : ${Math.floor(2.9)}`); // floor of number means round down
+
+console.log(`Math random number : ${Math.random()}`); // random number between 0 to 1
+console.log(`Math random number : ${Math.floor((Math.random() * 9999) + 1)}`); // random number between 1 to 10000
